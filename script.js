3213a2fee51988b7cead4182305486a00be58989
ritesh-gitwork <riteshmca.321@gmail.com>
2025-02-06 23:45:50 +0530

    add file

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const title = document.querySelectorAll("h4")[1];
-title.innerText = "not going gym";
-console.log(title);
+let ctr = 1;
+
+function deleteTodo(index) {
+  const delEle = document.getElementById(index);
+  delEle.parentNode.removeChild(delEle);
+}
+
+function addTodo() {
+  const inputEl = document.querySelector("input");
+  const value = inputEl.value;
+
+  const newDivEle = document.createElement("div");
+  newDivEle.setAttribute("id", ctr);
+
+  newDivEle.innerHTML =
+    "<div>" +
+    value +
+    '</div><button onclick="deleteTodo(' +
+    ctr +
+    ')">Delete</button>';
+
+  document.querySelector("body").appendChild(newDivEle);
+  ctr = ctr + 1;
+}
